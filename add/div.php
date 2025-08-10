69c9a1f8441c29870a1bfc85ce8b73aa8f3acd25
Worada Tanunat <157151293+Woradoii@users.noreply.github.com>
2025-08-11 02:33:19 +0700

    Update div.php

--- add/div.php
+++ add/div.php
@@ -24,20 +24,42 @@ require '../config/config.php';
 
   <div class="content">
     <h2>Division</h2>
-
     <div class="add_info">
       <div class="content">
-        <h3>เพิ่มข้อมูล</h3>
-        <form class="addinfo_form">
-          <label for="divname">Division name:</label>
-          <input type="text" id="divname" name="divname" required><br>
-          <label for="hdivname" id="hname_label">Head name:</label>
-          <input type="text" id="hdivname" name="hdivname" required>
-        </form>
-        <a href="#"><button class="btn" id="confirm" type="submit">ยืนยัน</button></a>
-        <a href="../main/division.php"><button class="btn" id="cancel">ยกเลิก</button></a>
+
+        <?php if ($_SERVER["REQUEST_METHOD"] === "POST") : ?>
+          <?php
+          $divname = mysqli_real_escape_string($connect, $_POST["divname"]);
+          $hdivname = mysqli_real_escape_string($connect, $_POST["hdivname"]);
+          $sql = "INSERT INTO division (NAME,HEAD) VALUE ('$divname','$hdivname')";
+          $result = mysqli_query($connect, $sql);
+          ?>
+          <?php if ($result) : ?>
+            <div class="boxform">
+              <h4>บันทึกเรียบร้อย</h4>
+              <a href="../main/division.php"><button class="btn" id="cancel">Go Back</button></a>
+            </div>
+          <?php else : ?>
+            <h4>บันทึกผิดพลาด</h4>
+            <a href="pj.php">เพิ่มใหม่</a>
+          <?php endif; ?>
+        <?php else : ?>
+          <div class="boxform">
+            <form class="addinfo_form" method="post" action="">
+              <label for="divname">Division name:</label>
+              <input type="text" id="divname" name="divname" required><br>
+              <label for="hdivname" id="hname_label">Head name:</label>
+              <input type="text" id="hdivname" name="hdivname" required><br>
+              <button class="btn" id="confirm" type="submit">ยืนยัน</button>
+            </form>
+
+            <a href="../main/division.php"><button class="btn" id="cancel">ยกเลิก</button></a>
+          </div>
+        <?php endif; ?>
+
       </div>
 
-
     </div>
+
+  </div>
 </body>
